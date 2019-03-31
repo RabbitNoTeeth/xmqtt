a05a280d9680920addff8af462909a1a842ecf85
liuxindong <864426487@qq.com>
2019-03-31 12:37:01 +0800

    删除测试代码

--- src/main/java/fun/bookish/xmqtt/mqtt/util/TopicNameParser.java
+++ src/main/java/fun/bookish/xmqtt/mqtt/util/TopicNameParser.java
@@ -16,10 +16,4 @@ public class TopicNameParser {
         return Pattern.compile(name);
     }
 
-
-    public static void main(String[] args) {
-        Pattern p1 = convert("xd/AnalogACQ/+/upstream/+");
-        Pattern p2 = Pattern.compile("xd/AnalogACQ/(\\w)+/upstream/(\\w)+");
-        System.out.println("xxx");
-    }
 }
